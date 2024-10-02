1fffba5225ba1c600e055ef0e35698abd358f4bd
Jingtian Li <jingtianli@gmail.com>
2024-10-02 12:23:17 -0500

    added qrcodes combine to png functionality

--- src/qrCodeGen.py
+++ src/qrCodeGen.py
@@ -2,9 +2,23 @@ import qrcode
 import os
 from consts import GetBoothNameTable, GetOutputDir, GetAdminAccessCode, GetAssetDir
 from PIL import Image
+import math
 
 def GetQrCodeAssetPath():
-    return os.path.join(GetAssetDir(), "qrcodeIcons") 
+    path = os.path.join(GetAssetDir(), "qrcodeIcons") 
+    path = os.path.normpath(path)
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+    return path
+
+def GetQrCodeOutputPath():
+    path = os.path.join(GetOutputDir(), "qrcodes")
+    path = os.path.normpath(path)
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+    return path
 
 def GetIconWithName(name):
     path = os.path.join(GetQrCodeAssetPath(), name+".png")
@@ -23,6 +37,20 @@ def GenerateAllQrCodes():
 def GetServerURL():
     return "http://3.137.157.79:8501"
 
+def GetExistingQrCodes():
+    qrCodeNames = os.listdir(GetQrCodeOutputPath())
+    qrCodePaths = []
+    for name in qrCodeNames:
+        qrCodePath = os.path.join(GetQrCodeOutputPath(), name)
+        qrCodePaths.append(os.path.normpath(qrCodePath))
+
+    return qrCodePaths
+
+def RemovePreviousQrCodes():
+    for qrCode in GetExistingQrCodes():
+        if not os.path.isdir(qrCode):
+            os.remove(qrCode)
+    
 def GenerateQrCode(codeFileName, data):
     
     # Create a QR code object
@@ -51,6 +79,37 @@ def GenerateQrCode(codeFileName, data):
         qrCodeImg.paste(qrCodeCenterIcon, iconPos, mask = qrCodeCenterIcon)
 
     # Save the image file
-    qrCodeImg.save(os.path.join(GetOutputDir(), codeFileName+".png"))
+    qrCodeImg.save(os.path.join(GetQrCodeOutputPath(), codeFileName+".png"))
 
-GenerateAllQrCodes()
+def CombineQrCodesIntoPdf():
+    qrCodePaths = GetExistingQrCodes()
+    images = [Image.open(image) for image in qrCodePaths]
+    images[0].save(os.path.join(GetQrCodeOutputPath(),'allQrCodes.pdf'), save_all=True, append_images=images[1:])
+
+def CombineQrCodeIntoImage(numOfColums = 3):
+    qrCodePaths = GetExistingQrCodes()
+    images = [Image.open(image) for image in qrCodePaths]
+
+    imageCount = len(images)
+    numOfRows = math.ceil(imageCount / numOfColums)
+
+    totalWidth = max(img.width for img in images) * numOfColums
+    totalHeight = max(img.width for img in images) * numOfRows
+
+    combinedImage = Image.new("RGB", (totalWidth, totalHeight), color = (255,255,255))
+
+    currentIndex = 0
+    for y in range(numOfRows):
+        for x in range(numOfColums):
+            print(f"x is: {x}, y is: {y}")
+            if currentIndex < len(images):
+                image = images[currentIndex]
+                combinedImage.paste(image, (x * image.width, y * image.height))
+                currentIndex+=1
+
+    combinedImage.save(os.path.join(GetQrCodeOutputPath(), "allQrCodes.png"))
+
+if __name__ == "__main__":
+    RemovePreviousQrCodes()
+    GenerateAllQrCodes()
+    CombineQrCodeIntoImage()
