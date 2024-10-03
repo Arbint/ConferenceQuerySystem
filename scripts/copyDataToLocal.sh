2e933a1a74b824bee2b4fb3598117fd0c17e260b
Jingtian Li <jingtianli@gmail.com>
2024-10-03 13:27:56 -0500

    added copy data to local script that asks the server to send the data back to local machine

--- scripts/copyDataToLocal.sh
+++ scripts/copyDataToLocal.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+ssh -i ../upgrade_sshkey.pem ec2-user@3.137.157.79 'docker cp upgradeboothregister:/app/src/data.db ~/data.db'
 scp -i ../upgrade_sshkey.pem ec2-user@3.137.157.79:~/data.db ../data.db 
