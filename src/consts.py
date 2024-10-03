2ea9a87a62dfc6097990bfc08086a191921dfc6a
Arbint <cg.jingtianli@gmail.com>
2024-10-02 19:26:41 -0500

    added more categories

--- src/consts.py
+++ src/consts.py
@@ -19,9 +19,13 @@ def GetOutputDir():
 
 def GetBoothNameTable():
     return {
-            '2b7d2f56fbaf5f56':"Animation",
-            'dd4d10e605e54b48':"Modeling",
-            'db702f38d9ae126e':"Programming"
+            'caf414ad66ab482c':"Ballroom_Animation",
+            '983ebf1830cd4fd6':"Ballroom_Modeling",
+            'fc9af05e1be9ad90':"Ballroom_Programming"
+            '9345b7ac1ebf36fb':"Animation_Interactive"
+            '3b35daf3e310fbbe':"Animation_Demo"
+            '393d01f7ce7ee1a4':"Modeling_Interactive"
+            'b20e98164a4df71d':"Modeling_Demo"
     }
 
 def GetAdminAccessCode():
