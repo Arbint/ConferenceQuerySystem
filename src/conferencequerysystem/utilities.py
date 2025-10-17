c11f52fce659cc15688dfd7924b17b0b2df5e315
Arbint <cg.jingtianli@gmail.com>
2025-10-17 11:34:47 -0500

    improved url gen to allow user defined base url

---
+++ src/conferencequerysystem/utilities.py
@@ -0,0 +1,47 @@
+from tkinter import simpledialog, Tk
+import os
+import sys
+from urllib.parse import urlparse
+
+def SupprtGUI():
+    # On Windows and macOS, GUI usually works by default.
+    if sys.platform.startswith("win") or sys.platform == "darwin":
+        return True
+
+    # On Linux/Unix — check for DISPLAY environment variable.
+    return "DISPLAY" in os.environ
+
+def IsValidURL(url):
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
+    
+
+def GetURLFromUser():
+    url = AskForURL() 
+    while not IsValidURL(url):
+        url = AskForURL("Invalid URL")
+
+    return url
+
+
+def AskForURL(msg=""):
+    if SupprtGUI():
+        root = Tk()
+        root.withdraw()
+        
+        label = "Please Enter a bse URL:"
+        if msg != "":
+            label = f"{msg}, {label}"
+
+        url = simpledialog.askstring(msg, label)
+
+        root.destroy()
+        return url
+    else:
+        print(msg)
+        return input(f"Player Enter a base URL:")
+
+
