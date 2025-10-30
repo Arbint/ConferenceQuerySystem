52399fb4bd3e17ce6b73eb21e1a6cb9838be1f62
Jingtian Li <jingtianli@jt-office-macmini-ad-408.ad.uiwtx.edu>
2025-10-30 11:32:57 -0500

    fixed some typos

--- src/conferencequerysystem/userClient.py
+++ src/conferencequerysystem/userClient.py
@@ -35,8 +35,8 @@ class App:
     def ShowMainGreeting(self):
         # st.title("UPGRADE CONFERENCE")
         st.image(self.GetConferenceLogoPath())
-        st.write("Tnank you for joining UPGRADE Conference 2025!")
-        st.write("Join the scanvenger hunt by visiting the booths and scanning the QR codes provided at each booth.")
+        st.write("Thank you for joining UPGRADE Conference 2025!")
+        st.write("Join the scavenger hunt by visiting the booths and scanning the QR codes provided at each booth.")
 
         st.subheader("Conference Map")
         st.image(self.GetConferenceMapPath())
